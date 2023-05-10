64935c225a8b9fd5bfbaf42ca816a934add6b0d8
Binn Kim <codepanda95@gmail.com>
2023-05-10 12:00:43 +0900

    Add data_sources to retrieve df of each dataset easily

---
+++ data_sources.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+DATA_DIR = '../data'
+
+def get_train_data():
+    return pd.read_csv(f'{DATA_DIR}/train_data/train_task_3_4.csv')
+
+def get_answer_data():
+    return pd.read_csv(f'{DATA_DIR}/metadata/answer_metadata_task_3_4.csv')
+
+def get_question_data():
+    return pd.read_csv(f'{DATA_DIR}/metadata/question_metadata_task_3_4.csv')
+
+def get_student_data():
+    return pd.read_csv(f'{DATA_DIR}/metadata/student_metadata_task_3_4.csv')
+
+def get_subject_data():
+    return pd.read_csv(f'{DATA_DIR}/metadata/subject_metadata.csv')
+
+
